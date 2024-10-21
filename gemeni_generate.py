ece0bbbf3bef66935f7ce0b286af3aa53122bc65
monstah123 <muscle40@gmail.com>
2024-10-20 23:04:45 -0400

    Update gemeni_generate.py

--- gemeni_generate.py
+++ gemeni_generate.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 # Configure the GEMINI LLM
-genai.configure(api_key='AIzaSyBaCcZeAhcYDj1A2Uk1hXWQYFTPOEEa_Zc')
+genai.configure(api_key='')
 model = genai.GenerativeModel('gemini-pro')
 
 #basic generation
